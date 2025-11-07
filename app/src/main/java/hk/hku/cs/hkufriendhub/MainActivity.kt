8ae21eaae68f10c1024b98d81fffa0e9a3fb7c46
wonghym <wonghymatt@gmail.com>
2025-11-07 22:59:31 +0800

    set up navbar

--- app/src/main/java/hk/hku/cs/hkufriendhub/MainActivity.kt
+++ app/src/main/java/hk/hku/cs/hkufriendhub/MainActivity.kt
@@ -1,20 +1,43 @@
 package hk.hku.cs.hkufriendhub
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navBar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        loadFragment(MainFragment())
+        navBar = findViewById<BottomNavigationView>(R.id.nav_bar)
+        navBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_main -> {
+                    loadFragment(MainFragment())
+                    true
+                }
+                R.id.nav_forum -> {
+                    loadFragment(ForumFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
         }
+    }
+
+    fun loadFragment(fragment: Fragment){
+        val manager: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction = manager.beginTransaction()
+        ft.replace(R.id.frame_container, fragment)
+        ft.commitAllowingStateLoss()
     }
 }
